0e089848fcd7b77a4afc9b0ccf4de55f34b3299d
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2025-07-18 23:19:17 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,9 @@ app=createApp()
 
 CORS(app,supports_credentials=True, origins={r'/*':{"origins":
     [
-      os.getenv("API_GATEWAY_URL")]
+      os.getenv("API_GATEWAY_URL"),
+     os.getenv("API_GATEWAY_URL_CERTIFICATE")
+    ]
     }})
 # if __name__=='__main__':
 #    app.run(port=os.getenv('PORT'),debug=True)
